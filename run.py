49533c44768c9ba31317320b43615c534ad68966
Aaron Watt <aaron@acwatt.net>
2021-12-27 21:21:00 -0800

    Added timezone fix and checking hourly average.

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 """Module Docstring"""
 
 # Built-in Imports
+import pandas as pd
 # Third-party Imports
 # Local Imports
 import acwatt_syp_code.analyze.maps as am
@@ -11,7 +12,18 @@ from acwatt_syp_code.utils.config import PATHS
 
 if __name__ == "__main__":
     # Get available sensor locations from  from purpleair.network.SensorList API wrapper
-    sensor_df = pad.load_current_sensor_data()
+    # sensor_df = pad.load_current_sensor_data()
+    # Get available sensor locations from thingsspeak API directly
+    sensors = [25999] #, 22751]
+    df_pa = pad.pa_request(sensors[0])
+    df_ts = pad.ts_example()
+    dfs = [pad.dl_sensor_weeks(id_, date_start='2021-12-25') for id_ in sensors]
+    df_total = pd.concat(dfs).sort_values('created_at')
+    df_total['datetime'] = pd.to_datetime(df_total['created_at'])
+    df_total.index = df_total['datetime']
+    df_hourly = df_total.resample('H').mean()
+    dfs = [pad.dl_sensor_weeks(id_, date_start='2021-12-25', average=60) for id_ in sensors]
+    df_total2 = pd.concat(dfs).sort_values('created_at')
     # Make sensor plots
     # am.make_all_sensor_maps(sensor_df)
     # Save sensor dataframe
