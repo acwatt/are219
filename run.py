a0bebe58629add6fc2bb29c37b8d4f30c1318d5b
acwatt laptop <aaron@acwatt.net>
2022-01-17 22:47:01 -0800

    Adding logging to run script

--- run.py
+++ run.py
@@ -3,6 +3,8 @@
 """Module Docstring"""
 
 # Built-in Imports
+import logging
+
 import pandas as pd
 # Third-party Imports
 # Local Imports
@@ -11,8 +13,10 @@ import acwatt_syp_code.build.purpleair_download as pad
 from acwatt_syp_code.utils.config import PATHS
 from acwatt_syp_code.build.aws import lambda_services
 
+logger = logging.getLogger(__name__)
+
 if __name__ == "__main__":
-    print('START')
+    logger.info('START')
     pad.dl_us_sensors()
     # lambda_services.save_pa_data_to_s3()
-    print('DONE')
+    logger.info('DONE')
